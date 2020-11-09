e8ee59e2c46a29e8218dd5fcb62e784080436a77
Timothy-LiuXuefeng <liuxf19@163.com>
2020-11-09 21:47:57 +0800

    修改了第六次作业

--- homework1/6.5.c
+++ homework1/6.5.c
@@ -1,75 +1,48 @@
-#pragma warning(disable:4996)
+#pragma warning(disable : 4996)
 #include <stdio.h>
-#define NUMBER 20
-int main() {
-	int numOfMin = 0 , numOfMax = 0 , sumOfPrime = 0 ;
-	int Min[10] = { 0 }, Max[10] = { 0 };
+#define NUMBER 1000
+
+int isPrime(int n)
+{
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main()
+{
+	int numOfMin = 0, numOfMax = 0, sumOfPrime = 0;
+	int maxMinPrime = 0;
 
 	//Find the minimum 10 primes.
 
-	for (int i = 2; i < NUMBER; ++i) {
-		int flag = 0;
-		for (int j = 2; j * j <= i; ++j) {
-			if (i % j == 0) {
-				flag = 1;
-				break;
-			}
-		}
-		if (flag == 0) {
-			Min[numOfMin] = i;
+	printf("最小素数：");
+	for (int i = 2; i <= NUMBER && numOfMin < 10; ++i)
+	{
+		if (isPrime(i))
+		{
 			++numOfMin;
-		}
-		if (numOfMin == 10) break;
-	}
-
-	//Not exists 10 primes.
-
-	if (numOfMin < 10) {
-		numOfMax = 0;
-	}
-	else {
-
-		//Find the maximum 10 primes.
-
-		for (int i = NUMBER - 1; i >1; --i) {
-			int flag = 0;
-			for (int j = 2; j * j <= i; ++j) {
-				if (i % j == 0) {
-					flag = 1;
-					break;
-				}
-			}
-			if (flag == 0) {
-				Max[numOfMax] = i;
-				++numOfMax;
-			}
-			if (numOfMax == 10) break;
+			sumOfPrime += maxMinPrime = i;
+			printf("%d, ", i);
 		}
 	}
-	//去除重复。
+	putchar('\n');
 
-		if (Min[numOfMin - 1] >= Max[numOfMax - 1]) {
-			int i = numOfMax - 1;
-			while (Max[i] <= Min[numOfMin - 1] && i >= 0) {
-				Max[i] = 0;
-				--numOfMax;
-				--i;
-			}
+	printf("最大素数：");
+	for (int i = NUMBER; numOfMax < 10 && i > maxMinPrime; --i)
+	{
+		//printf("%d--", i);
+		if (isPrime(i))
+		{
+			++numOfMax;
+			sumOfPrime += i;
+			printf("%d, ", i);
 		}
-		for (int i = 0; i < 10; ++i) {
-			sumOfPrime += Min[i];
-			sumOfPrime += Max[i];
-		}
-
-	//Output the 20 primes.
-
-	printf("最小素数:");
-	for (int i = 0; i < numOfMin - 1; ++i) printf("%d,", Min[i]);
-	printf("%d\n", Min[numOfMin - 1]);
-	printf("最大素数:");
-	for (int i = 0; i < numOfMax - 1; ++i) printf("%d,", Max[i]);
-	if(numOfMax > 0) printf("%d\n", Max[numOfMax - 1]);
-	printf("\n");
-	printf("素数之和:%d",sumOfPrime);
+	}
+	putchar('\n');
+	
+	printf("素数之和:%d\n", sumOfPrime);
 	return 0;
 }
