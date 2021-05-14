aeab47cf17cc2ae505988d00479baada37847b70
Liu Xuefeng <liuxf19@163.com>
2021-05-15 00:27:22 +0800

    Correct inline functions in C

--- homework1/8.4.c
+++ homework1/8.4.c
@@ -1,13 +1,16 @@
-/*#pragma warning (disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
-inline int Isdigit(char ch) {
+
+static inline int Isdigit(char ch) {
 	if (ch <= '9'&& ch >= '0') return 1;
 	return 0;
 }
-inline int Isalpha(char ch) {
+static inline int Isalpha(char ch) {
 	if (ch <= 'z' && ch >= 'a' || ch <= 'Z' && ch >= 'A') return 1;
 	return 0;
 }
+
 int main() {
 	int numOfDig = 0, numOfAlpha = 0;
 	char c;
@@ -18,4 +21,4 @@ int main() {
 	}
 	printf("The number of letter(s) is %d;\nThe number of digit(s) is %d; \n", numOfAlpha, numOfDig);
 	return 0;
-}*/
+}
